82038b9e7a9852963ddfb42fba187eb8e59fb671
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2022-01-19 16:42:37 -0500

    trying to add sort feature

--- products-app/src/components/Products.jsx
+++ products-app/src/components/Products.jsx
@@ -1,27 +1,19 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import { Card, Container, Row, Col } from "react-bootstrap";
+import { Dropdown, DropdownButton, Card, Container, Row, Col } from "react-bootstrap";
+const _ = require("lodash");
 
 export default function Products() {
   const [products, setProducts] = useState([]);
-  //value of the search field 
-  // const [searchInput, setSearchInput] = useState("");
-  //search result
   const [foundProducts, setFoundProducts] = useState(products);
-  console.log(products);
-  
-  // const filteredProducts = products.filter(
-  //   product => {
-  //     return (
-  //       product
-  //         .name
-  //         .includes(searchInput.toLowerCase())
-  //     )
-  //   }
-  // )
 
-    //filter function for search results
+  const sortedAZ = _.sortBy(foundProducts, "name");
+  const sortedZA = _.orderBy(foundProducts, ["name"], ["desc"])
+  const [sortParam, setSortParam] = useState(foundProducts);
+  
+
+  //filter function for search results
   const filter = (e) => {
     const search = e.target.value;
     console.log(search);
@@ -30,32 +22,22 @@ export default function Products() {
         product.name.toLowerCase().includes(search.toLowerCase())
       )
     });
-    console.log(results);
     setFoundProducts(results);
-    console.log(foundProducts);
   }
-
-  // console.log(foundProducts);
-
-  // const searchItems = (searchValue) => {
-  //   setSearchInput(searchValue);
-  //   console.log(searchInput);
-  //   products.filter((item) => {
-  //     return Object.values(item).join('').toLowerCase().includes(searchInput.toLowerCase())
-  //   })
-  // }
 
   useEffect(() => {
     const fetchProducts = async () => {
       const api = "https://products-api-01.herokuapp.com/api/products";
       axios.get(api)
         .then(data => {
-          // console.log(data.data);
           setProducts(data.data);
+          setFoundProducts(data.data);
+          setSortParam(data.data);
         })
     }
     fetchProducts();
   }, []);
+
 
   return (
     <div>
@@ -64,19 +46,24 @@ export default function Products() {
           icon="search"
           type="search"
           placeholder="Search..."
-          // onChange={handleChange}
           onChange={filter}
-          // onChange={(e) => searchItems(e.target.value)}
         />
       </div>
-      <div>
-      
-      </div>
 
-
+      <Dropdown>
+        <DropdownButton id="dropcown-btn" title="SORT BY:">
+          <Dropdown.Item onClick={()=>{setSortParam("sortedAZ")}}>Alphabetically, A-Z</Dropdown.Item>
+          <Dropdown.Item onClick={()=>{setSortParam("sortedZA")}}>Alphabetically, Z-A</Dropdown.Item>
+          <Dropdown.Item>Price, low to high</Dropdown.Item>
+          <Dropdown.Item>Price, high to low</Dropdown.Item>
+        </DropdownButton>
+      </Dropdown>
+  
       <Container className="grid">
         <Row>
-          {foundProducts.map((item) => {
+          {/* {foundProducts.map((item) => { */}
+          {/* {sortedAZ.map((item) => { */}
+          {sortParam.map((item) => {
             return (
               <Col xs="12" sm="6" lg="4" xl="3" key={item._id}>
                 <Link to={`/products/${item._id}`} style={{ textDecoration: 'none', color: "black"}}>
